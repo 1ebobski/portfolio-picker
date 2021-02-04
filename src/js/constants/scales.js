7229049c6e771ea8633c3bbd7eb51a5816f7b920
Fedor Lukhmanov <fedor.lukhmanov@ya.ru>
2021-02-04 17:25:57 +0300

    config and matrixes update + money scale change

--- src/js/constants/scales.js
+++ src/js/constants/scales.js
@@ -2,21 +2,19 @@
 // rows are "return +8%, drop -5%, return +13%, drop -10%", "return +20%, drop -15%", "return max, drop any"
 
 const RISK_MATRIX = [
-  [1, 2, 3, 3],
+  [1, 3, 3, 3],
   [2, 3, 4, 5],
   [2, 4, 5, 6],
-  [2, 4, 6, 7],
+  [2, 5, 6, 7],
 ];
 
 // keys are keys and values are money breakpoints in rubles
 
 const MONEY_SCALE = {
-  a: 1000,
-  b: 200000,
-  c: 300000,
-  d: 1000000,
-  f: 5000000,
-  e: 60000000,
+  a: 10000,
+  b: 500000,
+  c: 2000000,
+  d: 10000000,
 };
 
 export { RISK_MATRIX, MONEY_SCALE };
